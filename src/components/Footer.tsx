d7ebe85429a4a8313b52fc7f1961e3e2e638d9fe
SzymonG110 <gornikowskiszymon@gmail.com>
2022-07-28 08:36:07 +0200

    animation fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ export default class Footer extends Component {
     render(): JSX.Element {
 
         return (
-            <div className='mt-10 text-center p-7' id='footer'>
-                <h1 className='text-[20px] font-bold' data-aos='zoom-out-right' data-aos-anchor='#footer'>Szymon.ml</h1>
-                <p className='text-[15px]' data-aos='zoom-out-right' data-aos-anchor='#footer'>
+            <div className='mt-10 text-center p-7'>
+                <h1 className='text-[20px] font-bold' data-aos='zoom-out-right' data-aos-anchor='#footer-animation'>Szymon.ml</h1>
+                <p className='text-[15px]' data-aos='zoom-out-right' data-aos-anchor='#footer-animation'>
                     Stworzono przez: <a href='https://szymon.ml' target='_blank' className='hover:text-blue-300 duration-500 font-semibold'>szymon.ml</a> & <a href='https://rkuba.pl' target='_blank' className='hover:text-blue-300 duration-500 font-semibold'>rkuba.pl</a>
                 </p>
             </div>
