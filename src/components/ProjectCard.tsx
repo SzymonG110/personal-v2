c0322b56a54094ecc2c97d1ce8b8c9bf34292918
SzymonG110 <gornikowskiszymon@gmail.com>
2022-07-28 08:04:58 +0200

    fix links and full project json

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -46,7 +46,7 @@ export default class ProjectCard extends Component<Props, Readonly<{}>> {
 
     }
 
-    links(): JSX.Element | undefined {
+    links(): JSX.Element[] {
 
         const Page =
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
@@ -63,15 +63,14 @@ export default class ProjectCard extends Component<Props, Readonly<{}>> {
                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
             </svg>
 
-        for (const link of this.props.links)
-            return (
-                <a className='bg-[#A8A8A8] text-black rounded-xl inline-flex items-center px-3 py-1 hover:bg-[#dadada] duration-500' href={link.url}>
-                    {link.name === 'Strona' && Page}
-                    {link.name === 'Discord' && Discord}
-                    {link.name === 'GitHub' && GitHub}
-                    {link.name}
-                </a>
-            )
+        return this.props.links.map(link =>
+            <a className='bg-[#A8A8A8] text-black rounded-xl inline-flex items-center px-3 py-1 hover:bg-[#dadada] duration-500' href={link.url}>
+                {link.name === 'Strona' && Page}
+                {link.name === 'Discord' && Discord}
+                {link.name === 'GitHub' && GitHub}
+                {link.name}
+            </a>
+        )
 
     }
 
@@ -86,7 +85,7 @@ export default class ProjectCard extends Component<Props, Readonly<{}>> {
                 <div>
                     <h4 className='flex items-center text-xs'>
                         <div className='bg-[#ffffff] h-3 w-3 rounded-full flex justify-center items-center mr-1'>
-                            <div className={`bg-[${this.props.language.color}] h-2 w-2 rounded-full`} />
+                            <div className={`bg-[${this.props.language.color}] h-2 w-2 rounded-full`} style={{ background: this.props.language.color }} />
                         </div>
                         {this.props.language.name}
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-3 mr-1" viewBox="0 0 20 20" fill="currentColor">
@@ -102,7 +101,9 @@ export default class ProjectCard extends Component<Props, Readonly<{}>> {
                     </div>
                 </div>
 
-                {this.links()}
+                <div className='space-x-2'>
+                    {this.links()}
+                </div>
             </div>
         )
 
